7eded5ac8d011d9dca0dfa2339a583f4488e1228
rajashekharpatil007 <patil.rajaa@gmail.com>
2022-05-21 18:52:00 +0530

    Deployment ETE - CI/CD

---
+++ api/schemas.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
+
+class predictInput(BaseModel):
+    text:str
+    class Config:
+        orm_mode = True
+
+class predictCreate(BaseModel):
+    text:str
+  
